b9680c57dd623199aa4a56a4393fa803480ed0a1
Rahul6395 <102130290+Rahul6395@users.noreply.github.com>
2022-03-26 21:45:35 +0530

    Updates

---
+++ precache-manifest.e8b10ba62cedf757a6674124d965f86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40b1f85dbc4b8102f84",
+    "url": "/My-Portfolio/static/css/main.21872d1e.chunk.css"
+  },
+  {
+    "revision": "f40b1f85dbc4b8102f84",
+    "url": "/My-Portfolio/static/js/main.f40b1f85.chunk.js"
+  },
+  {
+    "revision": "d5ce9228864f6f8f91b1",
+    "url": "/My-Portfolio/static/js/1.d5ce9228.chunk.js"
+  },
+  {
+    "revision": "aa9d0237064e9f092376",
+    "url": "/My-Portfolio/static/js/runtime~main.aa9d0237.js"
+  },
+  {
+    "revision": "651eb6a7959c7270b2028db50f18fc7e",
+    "url": "/My-Portfolio/static/media/borderImg.651eb6a7.jpg"
+  },
+  {
+    "revision": "921e16f0697d4de1b493872253a106ff",
+    "url": "/My-Portfolio/index.html"
+  }
+];
